b96a7a6a9d8a0291a56c28f637f868d9a0c1f062
karlikp <pitera.karol@gmail.com>
2025-12-05 20:55:53 +0100

    feat: add stereo vision cameras to drone front

---
+++ scripts/post_start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+source /home/karol/ws/install/setup.bash # Point(.) is equel (source)
+export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$(ros2 pkg prefix uav_sim)/share/uav_sim/models_sdf
+
+mkdir -p /home/karol/.config/QGroundControl
+chown -R karol:karol /home/karol/.config/QGroundControl
+chmod -R u+rwX /home/karol/.config/QGroundControl
+chmod -R a+rw /home/karol/ws/src/sim_bringup/worlds || true
+
+
+
+colcon build
--- src/sim_bringup/build/sim_bringup/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/karol/ws/src/dron_sim/sim_bringup/install/sim_bringup'
